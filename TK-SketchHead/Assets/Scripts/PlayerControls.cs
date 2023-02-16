4e2d2eef48f153e2f47086ad62012307ee057ae2
cnladera <cnladera@gmail.com>
2023-02-16 14:51:54 -0800

    Tripp SketchHead

---
+++ TK-SketchHead/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    [Header("Default Down Speed")]
+    public float downSpeed = 20f;
+    [Header("Default Movement Speed")]
+    public float movementSpeed = 10f;
+    [Header("Default Directional Movement Speed")]
+    public float movement = 0f;
+        
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontak") * movementSpeed;
+        
+        if(movement < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
